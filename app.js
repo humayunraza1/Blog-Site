73f9aafd12277f8c525b91d94d9e224409945d32
Humayun Raza <humayunraza5@gmail.com>
2022-08-10 16:23:08 +0500

    added images feature to posts

--- app.js
+++ app.js
@@ -2,20 +2,38 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const fileUpload = require('express-fileupload');
 const ejs = require("ejs");
 var _ = require("lodash");
-var colorName = "";
-var fontColor = "black";
+var picture ="";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const posts=[];
 var isDark = "false";
 const app = express();
 
+const multer = require("multer");
+const path = require("path");
+
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) =>{
+      cb(null,__dirname+"/public/images");
+  },
+  filename: (req, file, cb) =>{
+      console.log(file);
+      cb(null, Date.now()+ path.extname(file.originalname));
+      console.log(file.filename);
+  }
+});
+
+
+const upload = multer({storage:storage});
 
 app.get("/",function(req,res){
   res.render("home",{Posts:posts, Dark: isDark});
@@ -30,18 +48,23 @@ app.get("/contact",function(req,res){
   res.render("contact",{Contact:contactContent, Dark:isDark});
 })
 
-app.post("/", function(req,res){
+app.post("/", upload.single("picture"),function(req,res){
   content = new String(req.body.postBody);
   Title = req.body.postTitle;
   Color = req.body.pColor;
   font = req.body.fColor;
-  
+  try{
+    picture = req.file.filename;
+  } catch(e){
+    console.log(e)
+  }
   const post = {
     title: Title,
     body: content,
     color: Color,
-    fontC: font
-  };
+    fontC: font,
+    pic: picture
+  }
   posts.push(post);
   res.redirect("/");
 })
